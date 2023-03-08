4351120378d5664c85264e611e831cdb2b9ad630
Drew Coker <drewrosecoker@gmail.com>
2023-03-08 14:26:09 -0500

    working on 'tinder' swipe feature

--- frontend/static/src/apps/views/UserMatch.js
+++ frontend/static/src/apps/views/UserMatch.js
@@ -1,9 +1,13 @@
 import { useState, useEffect } from "react";
 import UserMatchObject from "../components/UserMatchObject";
+import TinderCard from 'react-tinder-card'
+import '../styles/views.css'
 
 function UserMatch () {
     const [profiles, setProfiles] = useState([]);
+    const [lastDirection, setLastDirection] = useState()
 
+    console.log({lastDirection})
     useEffect(() => {
       const getProfiles = async () => {
         const response = await fetch(`/api_v1/profiles/`);
@@ -16,18 +20,62 @@ function UserMatch () {
       getProfiles();
     }, []);
   
-    const matchHTML = profiles.map((profile) => (
-      <div key={profile.id}>
-        <UserMatchObject {...profile} />
-      </div>
-    ));
+    const onSwipe = (direction) => {
+        console.log('You swiped: ' + 'right')
+    }
+      
+    const onCardLeftScreen = (myIdentifier) => {
+        console.log(myIdentifier + ' left the screen')
+    }
+
+    const swiped = (direction, nameToDelete) => {
+        console.log('removing: ' + nameToDelete)
+        setLastDirection(direction)
+      }
+    
+      const outOfFrame = (name) => {
+        console.log(name + ' left the screen!')
+      }
+
+    // const matchHTML = profiles.map((profile) => (
+    //   <div key={profile.id}>
+    //     <TinderCard 
+    //         className="pressable"
+    //         onSwipe={onSwipe} 
+    //         onCardLeftScreen={() => onCardLeftScreen('fooBar')} 
+    //         preventSwipe={['right', 'left']}>
+    //     <UserMatchObject {...profile} />
+    //     </TinderCard>
+    //   </div>
+    // ));
+
 
 
     return (
         <div>
-            {matchHTML}
+
+        <div className="header">
+            <h1>card container</h1>
         </div>
-    )
-}
+
+        <div className="user-object-placeholder">
+        <div>
+        {profiles.map((profile) =>
+        <TinderCard 
+        className="swipe" 
+        key={profile.id}
+        onSwipe={(dir) => swiped(dir, profile.username)}
+        onCardLeftScreen={() => outOfFrame(profile.username)}>
+        {profile.username}
+        </TinderCard>
+        )}
+        </div>
+        </div>
+        <div className="swipe-direction">
+        {lastDirection ? <h2 className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText' />}
+        </div>
+        </div>
+    );
+};
 
 export default UserMatch
