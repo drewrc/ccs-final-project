5bbc60441786459ff65ce03a83d4a89234d0c229
Drew Coker <drewrosecoker@gmail.com>
2023-03-13 11:23:45 -0400

    updated twilio api keys

--- accounts/send_sms.py
+++ accounts/send_sms.py
@@ -1,13 +1,13 @@
 from twilio.rest import Client
 import os
 
-AUTH_TOKEN = os.environ['auth_token']
-ACCOUNT_SID = os.environ['account_sid']
+TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
+TWILIO_ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
 
-client = Client(ACCOUNT_SID, AUTH_TOKEN)
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 # message = client.messages.create(
-#     to="+18648712004", 
+#     to="+18648712004",
 #     from_="+18888419554",
 #     body="Hello from Twilio")
 
@@ -20,5 +20,3 @@ client = Client(ACCOUNT_SID, AUTH_TOKEN)
 #         body=body)
 #     print("Notification sent to", to)
 #     return message.sid
-
-
