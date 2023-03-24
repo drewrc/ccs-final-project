13b211f0af53cbf40791b2ab2230cdc65966d5fd
Drew Coker <drewrosecoker@gmail.com>
2023-03-24 15:44:31 -0400

    updates frontend urls

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,6 +4,7 @@ from .views import IndexView
 app_name = "frontend"
 
 urlpatterns = [
-    #as view method we are calling...
+    # as view method we are calling...
     path("", IndexView.as_view(), name="index"),
+    path("<path:resource>/", IndexView.as_view()),
 ]
